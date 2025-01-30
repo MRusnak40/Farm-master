15e00498d0a95a09e615ec16e8a33a2b29715082
rusnak <rusnak.matyas07@gmail.com>
2025-01-30 10:59:51 +0100

    Added type of Animal Added arrys of Flowers and Animals added method for adding animals to array

--- Farm-master/src/Farm.java
+++ Farm-master/src/Farm.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Farm {
 
+    private int sizeOfFarm;
+    private ArrayList<Animal> seznamDobytku;
+    private ArrayList<Flower> seznamRostlin;
+Random rd =new Random();
+    public Farm() {
+        this.sizeOfFarm = 100;
+        seznamDobytku = new ArrayList<>();
+        seznamRostlin = new ArrayList<>();
+    }
+
+    public void addAnimal(int choose) {
+
+        if (choose==1){
+
+            seznamDobytku.add(new Guinipig("Guinipig","Pegas",Size.SMALL,"Dont trust him"));
+        }
+
+    }
 
 
+    public int getSizeOfFarm() {
+        return sizeOfFarm;
+    }
+
+    public void setSizeOfFarm(int sizeOfFarm) {
+        this.sizeOfFarm = sizeOfFarm;
+    }
 }
---
+++ Farm-master/src/Guinipig.java
@@ -0,0 +1,10 @@
+public class Guinipig extends Animal {
+
+
+    public Guinipig(String type, String name, Size size, String specialCare) {
+        super(type, name, size, specialCare);
+    }
+
+
+
+}
